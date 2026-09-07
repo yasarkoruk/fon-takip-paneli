6c503b232704cbc724efecd47ab0e84ae9a8a9f6
yasarkoruk <yasarkoruk@gmail.com>
2026-09-07 18:51:02 +0300

    Add files via upload

--- api/history.py
+++ api/history.py
@@ -145,6 +145,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(status)
         self.send_header("Content-Type", "application/json; charset=utf-8")
         self._cors()
-        self.send_header("Cache-Control", "public, max-age=900")
+        self.send_header("Cache-Control", "no-store")
         self.end_headers()
         self.wfile.write(body)
